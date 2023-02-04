76ec445957f8bde304ee27bbafb5252524bf8f2e
CZARNY77 <60876248+CZARNY77@users.noreply.github.com>
2023-02-04 23:01:49 +0100

    3 - In/Export continue

--- main/java/org/example/MainFrame.java
+++ main/java/org/example/MainFrame.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MainFrame extends DbConnector {
 
@@ -140,12 +148,49 @@ public class MainFrame extends DbConnector {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Wczytaj plik");
-                int result = fileChooser.showOpenDialog(Login.mainFrame);
+                fileChooser.setDialogTitle("Zapisz jako");
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Plik CSV", "csv");
+                fileChooser.setFileFilter(filter);
+                int result = fileChooser.showSaveDialog(Login.mainFrame);
                 if (result == JFileChooser.APPROVE_OPTION) {
                     File selectedFile = fileChooser.getSelectedFile();
                     String filePath = selectedFile.getAbsolutePath();
-                    // tutaj możesz wykonać import pliku na podstawie jego ścieżki
+
+                    //Zapisywanie pliku
+
+                    Connection conn = null;
+                    PreparedStatement stmt = null;
+
+                    try (CSVPrinter printer = new CSVPrinter(Files.newBufferedWriter(Paths.get(filePath)), CSVFormat.DEFAULT);){
+
+                        conn = connect();
+                        stmt = conn.prepareStatement(currentSQL);
+                        ResultSet rs = stmt.executeQuery();
+
+                        ResultSetMetaData rsmd = rs.getMetaData();
+                        int columnCount = rsmd.getColumnCount();
+                        String[] columnNames = new String[columnCount];
+
+                        for (int i = 0; i < columnCount; i++) {
+                            columnNames[i] = rsmd.getColumnName(i + 1);
+                        }
+                        printer.printRecord(columnNames);
+                        while (rs.next()) {
+                            ArrayList<String> row = new ArrayList<>();
+                            for (int i = 0; i < columnCount; i++) {
+                                row.add(rs.getString(i+1));
+                            }
+                            printer.printRecord(row);
+                        }
+
+                    } catch (Exception ex) {
+                        JOptionPane.showMessageDialog(null, "Błąd, sprawdz czy wybrany plik jest zamknięty!");
+                        ex.printStackTrace();
+                    }finally {
+                        Disconnect(conn, stmt);
+                    }
+
+
                 }
             }
         });
@@ -153,11 +198,44 @@ public class MainFrame extends DbConnector {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Zapisz jako");
-                int userSelection = fileChooser.showSaveDialog(Login.mainFrame);
+                fileChooser.setDialogTitle("Wczytaj plik");
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Plik CSV", "csv");
+                fileChooser.setFileFilter(filter);
+                int userSelection = fileChooser.showOpenDialog(Login.mainFrame);
                 if (userSelection == JFileChooser.APPROVE_OPTION) {
                     File fileToSave = fileChooser.getSelectedFile();
                     // tutaj możesz zapisać plik do wybranej lokalizacji
+
+                    Connection conn = null;
+                    PreparedStatement stmt = null;
+
+                    try (CSVParser parser = new CSVParser(Files.newBufferedReader(Paths.get(String.valueOf(fileToSave))), CSVFormat.DEFAULT.withFirstRecordAsHeader());) {
+                        conn = connect();
+                        String insertSQL = "INSERT INTO Produkty (ID, Nazwa, Waga, Cena_sprzedazy) VALUES (?, ?, ?, ?);\n" +
+                                "INSERT INTO Magazyn (ID_Produktu, Ilosc, Miejsce) VALUES (?, ?, ?);";
+                        stmt = conn.prepareStatement(insertSQL);
+
+                        for (CSVRecord record : parser) {
+                            stmt.setInt(1, Integer.parseInt(record.get("ID")));
+                            stmt.setString(2, record.get("Nazwa"));
+                            stmt.setBigDecimal(3, new BigDecimal(record.get("Waga")));
+                            stmt.setBigDecimal(4, new BigDecimal(record.get("Cena_sprzedazy")));
+
+                            stmt.setInt(5, Integer.parseInt(record.get("ID")));
+                            stmt.setInt(6, Integer.parseInt(record.get("Ilosc")));
+                            stmt.setString(7, record.get("Miejsce"));
+                            stmt.executeUpdate();
+                        }
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(null, "Błąd, sprawdz czy wybrany plik jest zamknięty!");
+                        throw new RuntimeException(ex);
+                    } catch (SQLException ex) {
+                        JOptionPane.showMessageDialog(null, "Błąd dane się powtarzają!");
+                        throw new RuntimeException(ex);
+                    }finally {
+                        Disconnect(conn, stmt);
+                    }
+
                 }
             }
         });
